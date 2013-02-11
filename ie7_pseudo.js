fd0aabb1d3ead9c7ffe69c5fae3fb495d8160be7
Steve De Jonghe <seutje@gmail.com>
2013-02-11 14:27:41 +0100

    Fix IIFE & make global
    Fix IIFE and make into callable function in global scope.

--- ie7_pseudo.js
+++ ie7_pseudo.js
@@ -1,57 +1,60 @@
 /*
-Created by Kevin Dees
-License: none (public domain)
+Heavily inspired by https://github.com/kevindees/ie7_pseudo_elements
 */
-
-(function() {
-    function set_element(rule, content, iee) {
-        if($(rule).hasClass(iee)) {
-            if(content) {
-                $(rule + ' > ' + iee).html(content);
+document.createElement('iea');
+document.createElement('ieb');
+(function($){
+  window.ie7pseudos =  function() {
+      function set_element(rule, content, iee) {
+        var $rule = $(rule);
+        $rule.each(function(i, el) {
+          var $this = $(this);
+          if ($this.hasClass(iee)) {
+            if (content) {
+              $this.find('> ' + iee).html(content);
             }
-        }
-        else {
-            $(rule).addClass(iee);
-            if(iee == 'ieb') {
-                $(rule).each(function(index) {
-                    $(this).prepend($(document.createElement(iee)).append( content ));
-                });
+          }
+          else {
+            $this.addClass(iee);
+            if (iee == 'ieb') {
+              $this.prepend($(document.createElement(iee)).html(content));
             }
             else {
-              $(rule).each(function(index) {
-                    $(this).append($(document.createElement(iee)).append( content ));
-                });
+              $this.append($(document.createElement(iee)).html(content));
             }
-        }
-    }
+          }
+        });
+      }
 
-    function make_content(content) {
-        if(content == "\"\"" || content == "\'\'" || content == null) {
-            content = ''; }
-        else if(content.indexOf('url') == 0) {
-            content = "<img src='"+content.replace(/url\(/g, '').replace(/\)/, '')+"' />";
-        }
-        else {
-            content = content.substring(0, content.length-1).substring(1);
-            document.createTextNode(content);
-        }
-        return content;
-    }
+      function make_content(content) {
+          if(content == "\"\"" || content == "\'\'" || content == null) {
+              content = ''; }
+          else if(content.indexOf('url') == 0) {
+              content = "<img src='"+content.replace(/url\(/g, '').replace(/\)/, '')+"' />";
+          }
+          else {
+              content = content.substring(0, content.length-1).substring(1);
+              document.createTextNode(content);
+          }
+          return content;
+      }
 
-    var css = document.styleSheets;
-    for(var i = 0, j = css.length; i < j; i++) {
-        var rules = css[i].rules;
-        for(var x = 0, y = rules.length; x < y; x++) {
-            var selector = rules[x].selectorText;
-            if(selector.indexOf("> ieb") > 0 || selector.indexOf("> iea") > 0) {
-                var regex = /\>\s?ie[ab]/g;
-                var ruleStr = selector.replace(regex, '').replace(/\:[a-z]*/g, '');
-                var content = rules[x].style.content;
-                content = make_content(content);
-                if(selector.indexOf("> ieb") > 0) { var iee = "ieb" }
-                else { var iee = 'iea'}
-                set_element(ruleStr, content, iee);
-            }
-        }
-    }
-})();
+      var css = document.styleSheets;
+      for(var i = 0, j = css.length; i < j; i++) {
+          var rules = css[i].rules;
+          for(var x = 0, y = rules.length; x < y; x++) {
+              var selector = rules[x].selectorText;
+              if(selector.indexOf("> ieb") > 0 || selector.indexOf("> iea") > 0) {
+                  var regex = /\>\s?ie[ab]/g;
+                  var ruleStr = selector.replace(regex, '').replace(/\:[a-z]*/g, '');
+                  var content = rules[x].style.content;
+                  content = make_content(content);
+                  if(selector.indexOf("> ieb") > 0) { var iee = "ieb" }
+                  else { var iee = 'iea'}
+                  set_element(ruleStr, content, iee);
+              }
+          }
+      }
+  };
+  window.ie7pseudos();
+})(jQuery);
